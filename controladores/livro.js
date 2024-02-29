e573753a61a19216d6bcee447f8e25a74b1cf566
João Sortudo <luckyjoey13@gmail.com>
2024-02-29 16:38:37 -0300

    Arquivos do curso

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,4 +1,4 @@
-const { getTodosLivros, getLivroById, insereLivro } = require('../servicos/livro')
+const { getTodosLivros, getLivroById, insereLivro, modificaLivro, deletarLivroPorId } = require('../servicos/livro')
 
 
 function getLivros (req, res) {
@@ -15,8 +15,15 @@ function getLivros (req, res) {
 function getLivro (req, res) {
     try {
         const id = req.params.id
-        const livro = getLivroById(id)
-        res.send(livro)
+
+        if (id && Number(id)){     
+            const livro = getLivroById(id)
+            res.send(livro)
+        }
+        else {
+            res.status(422)
+            res.send("Id inválido.")
+        }
     }
     catch(error) {
         res.status(500)
@@ -29,9 +36,48 @@ function postLivro (req, res) {
         const livroNovo = req.body
         insereLivro(livroNovo)
         res.status(201)
-        res.send('Livro inserido com sucesso!')
+        res.send('Livro inserido com sucesso.')
     }
     catch(error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function patchLivro(req, res) {
+    try {
+        const id = req.params.id
+
+        if (id && Number(id)){
+            const body = req.body
+    
+            modificaLivro(body, id)
+            res.send('Item modificado com sucesso.')
+        }
+        else {
+            res.status(422)
+            res.send("Id inválido.")
+        }
+    }
+    catch(error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function deleteLivro(req, res) {
+    try {
+        const id = req.params.id
+
+        if (id && Number(id)){
+            deletarLivroPorId(id)
+            res.send("Livro deletado com sucesso.")
+        }
+        else {
+            res.status(422)
+            res.send("Id inválido.")
+        }
+    } catch (error) {
         res.status(500)
         res.send(error.message)
     }
@@ -40,5 +86,7 @@ function postLivro (req, res) {
 module.exports = {
     getLivros,
     getLivro,
-    postLivro
+    postLivro,
+    patchLivro,
+    deleteLivro
 }
